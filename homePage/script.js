46d66f8868e00cb9edcf8d42c80ced6f31702ed7
vishnudk <vishnudk179@gmail.com>
2021-04-25 01:31:22 +0530

    home page

---
+++ homePage/script.js
@@ -0,0 +1,10 @@
+function hiFun() {
+    // alert("hello man");
+    // document.getElementById("menu-bar").style.transitionDelay = "2s";
+    if (document.getElementById("menu-bar").style.width == "15%")
+        document.getElementById("menu-bar").style.width = "0";
+    else {
+        document.getElementById("menu-bar").style.width = "15%";
+
+    }
+}
