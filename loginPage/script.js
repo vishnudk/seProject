95cf2c4e676a5d0bb26c2db7bcb31ef68075891f
vishnudk <vishnudk179@gmail.com>
2021-04-24 20:45:21 +0530

    login page

--- loginPage/script.js
+++ loginPage/script.js
@@ -1,14 +1,24 @@
 function changeForm(e) {
     if (document.getElementById("singup-btn").style.backgroundColor != "white" && e == 'up') {
         document.getElementById("singup-btn").style.backgroundColor = "white";
-        document.getElementById("singin-btn").style.backgroundColor = "rgb(158, 158, 158)";
+        document.getElementById("singin-btn").style.backgroundColor = "#475b62";
         // document.getElementById("submit-btn").style.margin - top="100%";
+
+        document.getElementById("singin-btn").style.color = "white"
+        document.getElementById("singup-btn").style.color = "black";
+
+
         document.getElementById("singup").style.display = "block";
         document.getElementById("singin").style.display = "none";
     }
     else if (e == "in" && document.getElementById("singup-btn").style.backgroundColor == "white") {
         document.getElementById("singin-btn").style.backgroundColor = "white"
-        document.getElementById("singup-btn").style.backgroundColor = "rgb(158, 158, 158)";
+        document.getElementById("singup-btn").style.backgroundColor = "#475b62";
+
+        document.getElementById("singin-btn").style.color = "black"
+        document.getElementById("singup-btn").style.color = "white";
+
+
         document.getElementById("singin").style.display = "block";
         document.getElementById("singup").style.display = "none";
 
