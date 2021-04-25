11dd81597309f885ee254d70dcf2a6893c41d92a
vishnudk <vishnudk179@gmail.com>
2021-04-25 13:12:11 +0530

    home page added notifications

--- homePage/script.js
+++ homePage/script.js
@@ -1,10 +1,37 @@
+var n = 0;
 function hiFun() {
     // alert("hello man");
     // document.getElementById("menu-bar").style.transitionDelay = "2s";
-    if (document.getElementById("menu-bar").style.width == "15%")
-        document.getElementById("menu-bar").style.width = "0";
+    if (document.getElementById("menu-bar").style.width == "20%") {
+        document.getElementById("menu-bar").style.width = "0%";
+    }
     else {
-        document.getElementById("menu-bar").style.width = "15%";
+        document.getElementById("menu-bar").style.width = "20%";
 
+    }
+}
+
+function checkNotification() {
+    if (n > 0) {
+        document.getElementById("not-no").style.opacity = 1;
+        document.getElementById("not-no").innerHTML = n;
+
+    }
+    else {
+        document.getElementById("not-no").style.opacity = 0;
+    }
+}
+
+function addNo() {
+    n = n + 1;
+
+}
+
+function showNoti() {
+    if (n > 0) {
+        document.getElementById("notifications-display").style.height = "auto";
+    }
+    else {
+        alert("no notifications !!");
     }
 }
