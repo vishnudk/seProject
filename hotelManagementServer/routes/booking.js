b3e25d7c939cbf836da30cdfd12f21a32b29c2b0
vishnudk <vishnudk179@gmail.com>
2021-05-09 09:48:40 +0530

    added the home page and the payment page and the booking page:

---
+++ hotelManagementServer/routes/booking.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f85b0acc15ef96b06150fec9056e5a47ea6de4cce39ef568c4d89a826fe6b7d5
+size 365
---
+++ hotelManagementServer/routes/payment.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a929223a977da22482227daa52c210c58593ff2af8b45847f5867a214f807337
+size 325
